c100d4a93d67e83457482d6df48d6e4e32fde75a
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-07-29 00:24:52 -0500

    Add model registration and retrieval methods

--- engine/registry.py
+++ engine/registry.py
@@ -2,4 +2,22 @@ class Registry:
 
     _models = {}
 
-    ...
+    @classmethod
+    def register(
+        cls,
+        model_class
+    ):
+
+        cls._models[
+            model_class.model_name
+        ] = model_class
+
+        return model_class
+
+    @classmethod
+    def get(
+        cls,
+        model_name
+    ):
+
+        return cls._models[model_name]
