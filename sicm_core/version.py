fd73a0577b013a4f129be25f6e9cbec8ef0df34b
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-07-28 23:42:19 -0500

    Add version information to version.py

---
+++ sicm_core/version.py
@@ -0,0 +1,11 @@
+"""
+Información de versión del proyecto.
+"""
+
+__title__ = "sicm-core"
+
+__version__ = "1.0.0-alpha.1"
+
+__author__ = "Patrocinio Valencia"
+
+__email__ = "edvalenciao@unal.edu.co"
