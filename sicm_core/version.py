df0fe8debbb8b4a383783fd62f7c0598b73a8857
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-07-29 00:49:49 -0500

    Update author name in version.py

--- sicm_core/version.py
+++ sicm_core/version.py
@@ -1,11 +1,13 @@
 """
-Información de versión del proyecto.
+Version information.
 """
+
+from __future__ import annotations
 
 __title__ = "sicm-core"
 
 __version__ = "1.0.0-alpha.1"
 
-__author__ = "Patrocinio Valencia"
+__author__ = "Edinson Patrocinio Valencia Omaña"
 
 __email__ = "edvalenciao@unal.edu.co"
